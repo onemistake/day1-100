245bf4bc49fab44efa877e90a058f885647259aa
onemistake <xuao2012@126.com>
2019-10-21 16:11:34 +0800

    first commit day3

---
+++ day3.py
@@ -0,0 +1,16 @@
+# import math
+#
+# #输入三条边，如果能构成三角形就计算周长和面积，如果不能就输出不能构成三角形
+# a = float(input("请输入第一条边长："))
+# b = float(input("请输入第二条边长："))
+# c = float(input("请输入第三条边长："))
+#
+# if a + b > c and a + c > b and b + c > a:
+#     print("周长：%.1f" % (a + b + c ))
+#     p = (a + b + c) / 2
+#     area = math.sqrt(p * (p - a) * (p - b) * (p - c))
+#     print("面积：%.1f" % area)
+# else:
+#     print("不能构成三角形")
+
+
